a9383ac28cafbc7495e849d96bd558a9528e28cb
hyunjin <hyunjin.jeong@toki.waseda.jp>
2018-08-19 16:12:22 +0900

    deal with memory inside a function.. and factorization and more

--- 065.py
+++ 065.py
@@ -14,50 +14,111 @@ The sum of digits in the numerator of the 10th convergent is 1+4+5+7=17.
 
 Find the sum of digits in the numerator of the 100th convergent of the continued fraction for e.
 '''
-class Fraction:
-	def __init__(self,val:float):
-		# self.n = num
-		# self.d = den
-		# self.val = num/den
+import math
+class Number:
+	def __init__(self,val:float = 0.0):
 		self.val = val
 		self.chain = []
+		self.fracpart = []
 
 	def __str__(self):
-		return f"{self.chain}"
+		return f"{self.val}"
+
 	def reciprocate(self):
-		# self.n, self.d = self.d, self.n
 		self.val = 1/self.val
 
-	def simplify(self):
-		pass
-
 	def next(self):
-		# i = self.n//self.d
 		i = self.val//1
 		self.chain.append(int(i))
-		self.val -= i
+		self.val = self.val - i
+		self.fracpart.append(self.val)
+
+		boollist = [closenuff(self.val, i) for i in self.fracpart[:-1]]
+		if any(boollist) and len(self.fracpart)>2:
+			ind = [i for i, x in enumerate(boollist) if x][0]
+			print(f"there's repetition!\t{self.chain[ind+1:]}")
+			# self.chain = f"{self.chain[:ind+1]};{self.chain[ind+1:]}"
+			return False
 		self.reciprocate()
-		if i in self.chain:
-			ind = self.chain.index(i)
-			print(f"reapeating {self.chain[ind:]}")
-			return 0
-		# self.n -= self.d*i
-		
+
+class Fraction(Number):
+	def __init__(self, n, d):
+		super().__init__(float(n)/d)
+		self.n = n
+		self.d = d		
+
+	def __add__(self, other):
+		top = self.n* other.d + self.d*other.n
+		bot = self.d*other.d
+		return Fraction(n=top, d=bot)
+
+	def simplify(self, li=[]):
+		bigger = self.n if self.n>self.d else self.d
+		for i in range(2,int(math.sqrt(bigger))+1):
+			pass
+
+	def reciprocate(self):
+		self.n, self.d = self.d, self.n
+
+	def __repr__(self):
+		return f"{self.n}/{self.d}"
+	def __str__(self):
+		return f"{self.n}/{self.d}"
+
+def makecf(li=[1, 2, 2, 2, 2, 2, 2]):
+	fchain=[]
+	for i in range(len(li)-1,0,-1):
+		cf = Fraction(n=li[i],d=1)		
+		for j in li[i-1::-1]:
+			cf.reciprocate()
+			# print(cf, i, j)
+
+			cf = Fraction(n=j,d=1) + cf
+		fchain.append(cf)
+	#last one at li[0]
+	fchain.append(Fraction(n=li[0],d=1))
+	fchain.reverse()
+	# print(fchain)
+	return fchain
+
+def closenuff(a,b,epsilon=1.e-7):		
+	if abs(a-b)<epsilon: return True
+	return False
+
+def factorize(n, li=None):
+	if li == None:
+		li = []
+	for i in range(2,int(math.sqrt(n))+1):
+		if n%i ==0:
+			li.append(i)
+			return factorize(n/i, li)
+	li.append(int(n))
+	return li
 
 
 def solve():
-	# num, den = [float(i) for i in input("frac: ").split('/')]
-	val = float(input("val: "))
-	f = Fraction(val)
-	# while f.n > f.d and f.n != 0:
-	while f.val > 1:
-		try: 
-			if f.next() == 0: break
-		except ZeroDivisionError:
-			break
+	# val = float(input("val: "))
+	val = math.e
+	print(val)
+	f = Number(val=val)
+	while f.val > 1 and len(f.chain)<100:
+		if f.next(): break
+	chain = makecf(f.chain)
 
-	print(f)
+	# print(chain[-1].simplify())
 
+	factorized = []
+	for i in str(chain[-1]).split('/'):
+		print(int(i))
+		factorized.append( factorize(int(i)) )
 
+	for i in factorized[0][::-1]:
+		print(i, factorized[1])
+		if i in factorized[1]:
+			factorized[0].remove(i)
+			factorized[1].remove(i)
+	print(factorized)
 if __name__ =="__main__":
-	solve()
+	# solve()
+	print(factorize(15))
+	print(factorize(15))
